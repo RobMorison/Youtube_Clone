7658d360d31b26015f6e543f82cf07513eb08799
Sara Morison <rob.morison32@gmail.com>
2023-03-09 12:00:38 -0500

    Created comment model + added comment app to installed apps + made comment migration

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    like = models.IntegerField()
+    dislikes = models.IntegerField()
