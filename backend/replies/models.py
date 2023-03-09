c018950378c9580f7f5fa4caf4dc4a4018966279
Sara Morison <rob.morison32@gmail.com>
2023-03-09 12:07:22 -0500

    Created replies model + Added replies app to settings + Migrated replies app

--- backend/replies/models.py
+++ backend/replies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication.models import User
+from comments.models import Comments
 
 # Create your models here.
+
+class Replies(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comments, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
