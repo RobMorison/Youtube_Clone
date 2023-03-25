afe20d7fdc04a7b3df07c043ca7d8930ad50d39a
Sara Morison <rob.morison32@gmail.com>
2023-03-25 13:50:08 -0400

    Added map to RelatedVideos component

--- frontend/src/components/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-const RelatedVideos = ({related_video}) => {
+const RelatedVideos = ({related_videos}) => {
 
-    console.log('related videos', related_video)
+    console.log('related videos', related_videos)
 
     return(
        <ul>
         
-        {related_video?.map((el) => (
+        {related_videos?.map((el) => (
             <img src ={el.snippet.thumbnails.default.url}/>
         ))}</ul>
      );
